7b65c49b45485af99fa8c063ea3ba09f0c0a14f4
Banggi Bima <banggibima@gmail.com>
2022-08-26 07:06:31 +0700

    Initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
 const Footer: FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <footer>
         <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-xs">
-          Copyright &copy; 2022
+          Copyright &copy; {currentYear}
           <a
             href="https://bbima.ml"
             className="ml-1 hover:text-sky-600"
